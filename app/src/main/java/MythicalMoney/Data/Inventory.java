d0c90eea3943a84e0bbb0b96ab88eb6ef916d94d
il798li <92280179+il798li@users.noreply.github.com>
2024-03-22 09:21:58 -0700

    Update Inventory.java

---
+++ app/src/main/java/MythicalMoney/Data/Inventory.java
@@ -0,0 +1,8 @@
+package Data.Inventory;
+
+public class Inventory {
+	public int [] items;
+	public static final int totalItems = Item;
+	
+	public Inventory (int [] items) {
+		int [] newItems = new int [12];
