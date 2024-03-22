4b726fb2d8da8ebad0a75c5a8a9e4076b6e0990f
il798li <92280179+il798li@users.noreply.github.com>
2024-03-22 09:29:16 -0700

    Update Inventory.java

--- app/src/main/java/MythicalMoney/Data/Inventory.java
+++ app/src/main/java/MythicalMoney/Data/Inventory.java
@@ -2,7 +2,16 @@ package Data.Inventory;
 
 public class Inventory {
 	public int [] items;
-	public static final int totalItems = Item;
+	public static final int totalItems = Item.items.size ();
 	
-	public Inventory (int [] items) {
-		int [] newItems = new int [12];
+	public Inventory () {
+		this.items = new int [totalItems];
+		for (int index = 0; index < items.size; index += 1) {
+			this.items [index] = 0;
+		}
+	}
+
+	public static Inventory fromJSON (JSONObject jsonObject) {
+		return null;
+	}
+}
