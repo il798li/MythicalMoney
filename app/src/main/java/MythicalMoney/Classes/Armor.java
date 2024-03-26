e804e92a80fd917701677c4eeea67c7d701de4be
il798li <92280179+il798li@users.noreply.github.com>
2024-03-26 09:49:45 -0700

    Update Armor.java
    started basic class

---
+++ app/src/main/java/MythicalMoney/Classes/Armor.java
@@ -0,0 +1,13 @@
+package MythicalMoney.Classes;
+
+public class Armor {
+	public int health;
+	public int defense;
+	public DisplayPlus display;
+
+	public Armor (int health, int defense, DisplayPlus display) {
+		this.health = health;
+		this.defense = defense;
+		this.display = display;
+	}
+}
