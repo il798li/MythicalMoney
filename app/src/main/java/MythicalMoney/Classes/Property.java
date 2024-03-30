b58b4fec993d3a58628eaeccda9cfa25c05b19e4
il798li <svpa2008@gmail.com>
2024-03-29 17:29:20 -0700

    Update Property.java
    added toList method

--- app/src/main/java/MythicalMoney/Classes/Property.java
+++ app/src/main/java/MythicalMoney/Classes/Property.java
@@ -1,5 +1,7 @@
 package MythicalMoney.Classes;
 
+import java.util.ArrayList;
+
 import MythicalMoney.Classes.Obtainable.Tool;
 
 public class Property {
@@ -7,10 +9,22 @@ public class Property {
     public DisplayPlus display;
     public Tool tool;
 
+    public static ArrayList <Property> properties = new ArrayList <Property> ();
+
     public Property (DisplayPlus display, Tool tool, int [] chances) {
         this.display = display;
         this.chances = chances;
         this.tool = tool;
+
+        properties.add (this);
     }
 
+    public static Property [] toList (final ArrayList <Property> propertyArrayList) {
+        final int size = propertyArrayList.size ();
+        Property [] propertyList = new Property [size];
+        for (int index = 0; index < size; index++) {
+            propertyList [index] = propertyArrayList.get (index);
+        }
+        return propertyList;
+    }
 }
