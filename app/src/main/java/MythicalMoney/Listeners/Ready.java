e657be6f8b1b6362e404b36a3231c764668f5ad1
il798li <svpa2008@gmail.com>
2024-04-17 22:06:08 -0700

    Update Ready.java
    added intermediate vars for better readability
    removed construction of Credits
    add ping commands to guild

--- app/src/main/java/MythicalMoney/Listeners/Ready.java
+++ app/src/main/java/MythicalMoney/Listeners/Ready.java
@@ -2,11 +2,12 @@ package MythicalMoney.Listeners;
 
 import MythicalMoney.Main;
 import MythicalMoney.Commands.Basic.Credits;
+import MythicalMoney.Commands.Basic.Ping;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
@@ -18,19 +19,15 @@ public class Ready extends ListenerAdapter {
     public void onReady (ReadyEvent readyEvent) {
         JDA jda = readyEvent.getJDA ();
 
-        final String name = jda.getSelfUser ().getName ();
+        User mm = jda.getSelfUser ();
+        String name = mm.getName ();
         Main.debug ("Successfully signed in as " + name + "!");
 
         mmGuild = jda.getGuildById (834113328459677747L);
-        
-        {
-            new Credits (jda);
-        }
-
-        mmGuild.upsertCommand (Credits.slashCommandData);
 
         CommandListUpdateAction commandListUpdateAction = mmGuild.updateCommands();
         commandListUpdateAction.addCommands (Credits.slashCommandData);
+        commandListUpdateAction.addCommands (Ping.slashCommandData);
         commandListUpdateAction.queue ();
     }
 }
