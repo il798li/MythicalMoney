9d1d61cf8aa0d4d12a2863a357359bcb167ef45a
il798li <svpa2008@gmail.com>
2024-04-17 18:00:44 -0700

    .

--- app/src/main/java/MythicalMoney/Commands/Basic/Credits.java
+++ app/src/main/java/MythicalMoney/Commands/Basic/Credits.java
@@ -2,20 +2,18 @@ package MythicalMoney.Commands.Basic;
 
 import MythicalMoney.Main;
 import MythicalMoney.Classes.Display;
-import MythicalMoney.Listeners.Ready;
 import MythicalMoney.Utility.DiscordUtility;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
-import net.dv8tion.jda.api.requests.RestAction;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
 public class Credits {
-    public static SubcommandData subcommandData;
 
-    public Credits () {
-        subcommandData = new SubcommandData("credits", "Mythical Money");
-        Ready.mmBaseCommand.addSubcommands (subcommandData);
+    public static SlashCommandData slashCommandData;
+
+    public Credits (JDA jda) {
+        slashCommandData = Commands.slash ("credits", "Mythical Money");
     }
 
     public static void execute (SlashCommandInteractionEvent slashCommandInteractionEvent) {
@@ -24,22 +22,10 @@ public class Credits {
         Main.debug (slashCommandInteractionEvent.getCommandString());
 
         final Display [] displays = {
-            new Display ("Inspiration", "My creation was inspired by @" + name (475315771086602241L, jda) + " and @" + name (697535361315766322L, jda) + "."),
-            new Display ("Branding", "My name, Mythical Money, was inspired by @" + name (694226805439070269L, jda) + ".")
+            new Display ("Inspiration", "My creation was inspired by " + DiscordUtility.display (475315771086602241L, slashCommandInteractionEvent) + " and " + DiscordUtility.display (697535361315766322L, slashCommandInteractionEvent) + "."),
+            new Display ("Branding", "My name was inspired by " + DiscordUtility.display (694226805439070269L, slashCommandInteractionEvent) + ".")
         };
 
-        DiscordUtility.deletable(slashCommandInteractionEvent, displays, true);
-    }
-
-    public static String name (final long id, JDA jda) {
-        RestAction <User> userRestAction = jda.retrieveUserById (id);
-        userRestAction.complete ();
-        User user = jda.getUserById (id);
-        if (user == null) {
-            Main.debug (id + " is a null user.");
-            return "UKNOWN_NAME";
-        }
-        final String name = user.getName ();
-        return name;
+        DiscordUtility.deletable(slashCommandInteractionEvent, displays, false);
     }
 }
--- app/src/main/java/MythicalMoney/Commands/Basic/Settings.java
+++ app/src/main/java/MythicalMoney/Commands/Basic/Settings.java
@@ -1,5 +1,13 @@
 package MythicalMoney.Commands.Basic;
 
+import MythicalMoney.Listeners.Ready;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+
 public class Settings {
-    
+    public static SubcommandData subcommandData;
+
+    public Settings () {
+        subcommandData = new SubcommandData("settings", "Mythical Money");
+        Ready.mmBaseCommand.addSubcommands (subcommandData);
+    }
 }
