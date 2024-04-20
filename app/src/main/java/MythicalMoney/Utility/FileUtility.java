ab21172f51d999e65aac87ebe5f196931ad99ace
il798li <svpa2008@gmail.com>
2024-04-20 11:16:39 -0700

    Update FileUtility.java
    prototype writeFile method

--- app/src/main/java/MythicalMoney/Utility/FileUtility.java
+++ app/src/main/java/MythicalMoney/Utility/FileUtility.java
@@ -1,21 +1,17 @@
 package MythicalMoney.Utility;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 import MythicalMoney.Main;
 
 public class FileUtility {
     public static String readFile (String path) {
-        path = "app\\src\\main\\java\\MythicalMoney\\" + path;
-        File file;
-        try {
-            file = new File (path);
-        } catch (NullPointerException nullPointerException) {
-            Main.debug ("Exception: NullPointerException");
-            return "";
-        }
+        File file = file (path);
         Scanner scanner;
         try {
             scanner = new Scanner (file);
@@ -31,5 +27,31 @@ public class FileUtility {
 
         content = content.substring (0, content.length ());
         return content;
+    }
+
+    public static boolean writeFile (String content, String path) {
+        File file = file (path);
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter (file);
+            BufferedWriter bufferedWriter = new BufferedWriter (fileWriter);
+            bufferedWriter.write (content);
+            bufferedWriter.close ();
+            return true;
+        } catch (IOException ioException) {
+            Main.debug ("Could not write to file \"" + path + "\" due to an IOException.");
+            return false;
+        }
+    }
+
+    public static File file (String path) {
+        path = "app\\src\\main\\java\\MythicalMoney\\" + path;
+        File file = null;
+        try {
+            file = new File (path);
+        } catch (NullPointerException nullPointerException) {
+            Main.debug ("Could not open \"" + path + "\" due to a NullPointerException.");
+        }
+        return file;
     }
 }
