0299a53199e41c452ebf9c819d70a3aecd964c2c
il798li <svpa2008@gmail.com>
2024-04-20 17:45:11 -0700

    Create Save.java

---
+++ app/src/main/java/MythicalMoney/Commands/Restricted/Save.java
@@ -0,0 +1,39 @@
+package MythicalMoney.Commands.Restricted;
+
+import MythicalMoney.Classes.Display;
+import MythicalMoney.Data.Setting;
+import MythicalMoney.Listeners.Ready;
+import MythicalMoney.Utility.DiscordUtility;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+
+public class Save {
+    public static SlashCommandData slashCommandData = slashCommandData ();
+
+    public static SlashCommandData slashCommandData () {
+        SlashCommandData slashCommandData = Commands.slash ("save", "[Restricted] Save all current data to JSON.");
+        return slashCommandData;      
+    }
+
+    public static void execute (SlashCommandInteractionEvent slashCommandInteractionEvent) {
+        User user = slashCommandInteractionEvent.getUser ();
+        final boolean owner = Ready.owner (user);
+        if (owner == false) {
+            final Display errorDisplay = new Display ("Data Saving Error", "This command is only accessible by authorized users.");
+            final Display [] displays = {
+                errorDisplay
+            };
+            DiscordUtility.deletable(slashCommandInteractionEvent, displays, false);
+            return;
+        }
+
+        Setting.save ();
+        final Display successDisplay = new Display ("Data Save Success", "All data regarding Discord server settings was successfully saved!");
+        final Display [] displays = {
+            successDisplay
+        };
+        DiscordUtility.deletable(slashCommandInteractionEvent, displays, owner);
+    }
+}
