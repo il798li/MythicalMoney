64412e5a7bb7c4b204c41f833148a7d8ecaa18d0
il798li <svpa2008@gmail.com>
2024-04-23 21:04:38 -0700

    The Land class has been created with static methods to convert between JSONArray and Java memory. I still need to add instance helper methods.

---
+++ app/src/main/java/MythicalMoney/Data/Land.java
@@ -0,0 +1,33 @@
+package MythicalMoney.Data;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import MythicalMoney.Classes.Property;
+
+public class Land {
+    public Property [] properties;
+    
+    public static Land fromJSON (JSONArray jsonArray) {
+        final int length = jsonArray.length ();
+        Property [] properties = new Property [length];
+        for (int index = 0; index < length; index++) {
+            final String name = jsonArray.getString (index);
+            final Property property = Property.get (name);
+            properties [index] = property;
+        }
+
+        final Land land = new Land ();
+        land.properties = properties;
+        return land;
+    }
+
+    public static JSONArray toJSON (Land land) {
+        JSONArray jsonArray = new JSONArray(land.properties.length);
+        for (int index = 0; index < land.properties.length; index++) {
+            final Property property = land.properties [index];
+            jsonArray.put (index, property.display.name);
+        }
+        return jsonArray;
+    }
+}
