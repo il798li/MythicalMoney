ce8c9c7fa2972dc692aad3a5f3817a957711ae2f
il798li <svpa2008@gmail.com>
2024-04-27 16:32:58 -0700

    MM: Starter Tools
    I made the base 4 starter Tools. Every player will have these when the first register to MM.

--- app/src/main/java/MythicalMoney/Classes/Tool.java
+++ app/src/main/java/MythicalMoney/Classes/Tool.java
@@ -12,8 +12,13 @@ public class Tool {
     public ToolType toolType; 
 
     public static ArrayList <Tool> all = new ArrayList <Tool> ();
+
+    public static final Tool starterWeapon = Tool.weapon (new Display ("Starter Weapon", "starter weapon"), 60, new int [] {100, 0, 0, 0});
+    public static final Tool starterAxe = Tool.weapon (new Display ("Starter Axe", "starter axe"), 60, new int [] {100, 0, 0, 0});
+    public static final Tool starterHoe = Tool.hoe (new Display ("Starter Hoe", "starter hoe"), 60, new int [] {100, 0, 0, 0});
+    public static final Tool starterPickaxe = Tool.pickaxe (new Display ("Starter Pickaxe", "starter pickaxe"), 60, new int[] {100, 0, 0, 0});
     
-    private Tool (Display display, int cooldown, int [] chances, ToolType toolType) {
+    public Tool (Display display, int cooldown, int [] chances, ToolType toolType) {
         this.display = display;
         this.cooldown = cooldown;
         this.chances = chances;
