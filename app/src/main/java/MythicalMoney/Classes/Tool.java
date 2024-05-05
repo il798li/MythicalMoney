6d8f718a38468c1532efb4258e675ebf963a0956
il798li <svpa2008@gmail.com>
2024-05-04 18:04:31 -0700

    Code: Minor Optimizations
    I renamed the static get method to use an enhanced for-loop. All 4 static methods that generate different types of tools no longer use unnecessary intermediate variables.

--- app/src/main/java/MythicalMoney/Classes/Tool.java
+++ app/src/main/java/MythicalMoney/Classes/Tool.java
@@ -14,7 +14,7 @@ public class Tool {
     public static ArrayList <Tool> all = new ArrayList <Tool> ();
 
     public static final Tool starterWeapon = Tool.weapon (new Display ("Starter Weapon", "starter weapon"), 60, new int [] {100, 0, 0, 0});
-    public static final Tool starterAxe = Tool.weapon (new Display ("Starter Axe", "starter axe"), 60, new int [] {100, 0, 0, 0});
+    public static final Tool starterAxe = Tool.axe (new Display ("Starter Axe", "starter axe"), 60, new int [] {100, 0, 0, 0});
     public static final Tool starterHoe = Tool.hoe (new Display ("Starter Hoe", "starter hoe"), 60, new int [] {100, 0, 0, 0});
     public static final Tool starterPickaxe = Tool.pickaxe (new Display ("Starter Pickaxe", "starter pickaxe"), 60, new int[] {100, 0, 0, 0});
     
@@ -28,10 +28,8 @@ public class Tool {
     }
 
     public static Tool get (final String name) {
-        final int size = all.size ();
-        for (int index = 0; index < size; index++) {
-            final Tool tool = all.get (index);
-            final boolean nameMatch = tool.display.plural.equals (name);
+        for (final Tool tool : all) {
+            final boolean nameMatch = tool.display.plural.equals(name);
             if (nameMatch) {
                 return tool;
             }
@@ -40,22 +38,18 @@ public class Tool {
     }
 
     public static Tool weapon (Display display, int cooldown, int [] chances) {
-        Tool weapon = new Tool(display, cooldown, chances, ToolType.Weapon);
-        return weapon;
+        return new Tool(display, cooldown, chances, ToolType.Weapon);
     }
 
     public static Tool axe (Display display, int cooldown, int [] chances) {
-        Tool axe = new Tool(display, cooldown, chances, ToolType.Axe);
-        return axe;
+        return new Tool(display, cooldown, chances, ToolType.Axe);
     }
 
     public static Tool hoe (Display display, int cooldown, int [] chances) {
-        Tool hoe = new Tool(display, cooldown, chances, ToolType.Hoe);
-        return hoe;
+        return new Tool(display, cooldown, chances, ToolType.Hoe);
     }
 
     public static Tool pickaxe (Display display, int cooldown, int [] chances) {
-        Tool pickaxe = new Tool(display, cooldown, chances, ToolType.Pickaxe);
-        return pickaxe;
+        return new Tool(display, cooldown, chances, ToolType.Pickaxe);
     }
 }
